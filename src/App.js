bde4ecf9858e8ee67b76fcc81accf47115c3582f
VolksRat71 <nathanieljryan1994@gmail.com>
2019-10-04 15:18:09 -0700

    background rendering commencing component builds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import img from './assets/background.JPG';
+import styled from 'styled-components';
+
+const AppStyles = styled.div`
+  background-image: url(${img});
+  height: 2000px;
+`;
 
 function App() {
   return (
-    <div class="veiwport">
-      hey
-    </div>
+    <AppStyles>
+      <div class="veiwport">
+        hey
+      </div>
+    </AppStyles>
   );
 }
 
