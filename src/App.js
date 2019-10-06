bf93aa1da54a4f23e0b07ad1a9251de2b1d5ed1f
VolksRat71 <nathanieljryan1994@gmail.com>
2019-10-06 11:58:06 -0700

    Material layout rendering my name in the NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
-import 'react-mdl/extra/material.css';
-import 'react-mdl/extra/material.js';
+
+import {
+  Layout,
+  Header,
+  Navigation,
+  Drawer,
+  Content
+} from 'react-mdl';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Layout fixedHeader>
+        <Header title={<span><span style={{ color: '#ddd' }}>Nathaniel </span><strong>Ryan</strong></span>}>
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+            <a href="/">Link</a>
+          </Navigation>
+        </Drawer>
+        <Content />
+      </Layout>
     </div>
   );
 }
