f869a2bdb783cda43edf1fd8b0d0640ff5f1b799
VolksRat71 <nathanieljryan1994@gmail.com>
2019-10-06 16:51:11 -0700

    landing page rendering avatar descriptions and links

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import img from '../assets/clean.png';
+
 class LandingPage extends Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Landing Page
-                </h1>
+            <div style={{ width: '100%', margin: 'auto' }}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src={img}
+                            alt="landing avatar"
+                            className="landing-avatar"
+                        />
+                        <div className="banner-text">
+                            <h1>
+                                Full Stack JavaScript Developer
+                                <hr />
+                                <p>
+                                    HTML5 | CSS3 | JavaScript | ReactJS | React Native | NodeJS | Express | MongoDB | MySQL | Git
+                                </p>
+                                <div className="social-links">
+                                    <a href="mailto: nathanieljryan1994@gmail.com" title="nathanieljryan1994@gmail.com" target="_blank">
+                                        <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                    </a>
+                                    <a href="https://www.linkedin.com/in/nathaniel-ryan-88871817b" title="LinkedIn Nathaniel Ryan" target="_blank">
+                                        <i class="fa fa-linkedin" aria-hidden="true" />
+                                    </a>
+                                    <a href="https://www.freecodecamp.org/volksrat71" title="freeCodeCamp volksrat71" target="_blank">
+                                        <i class="fa fa-free-code-camp" aria-hidden="true" />
+                                    </a>
+                                    <a href="https://github.com/VolksRat71" title="Github VolksRat71" target="_blank">
+                                        <i class="fa fa-github" aria-hidden="true" />
+                                    </a>
+                                </div>
+                            </h1>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
