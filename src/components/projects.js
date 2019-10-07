b00064a1388d8e768b29544e1166fd0490902bc5
VolksRat71 <nathanieljryan1994@gmail.com>
2019-10-07 14:03:22 -0700

    Begain projects page build,  react mdl tabs implemented and working correctly, no content

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
-class Projects extends Component {
+import React from 'react';
+import { Tabs, Tab } from 'react-mdl';
+
+
+class Projects extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { activeTab: 2 };
+    }
+
     render() {
         return (
-            <div>
-                <h1>
-                    Projects
-                </h1>
+            <div className="demo-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>Frontend</Tab>
+                    <Tab>Backend</Tab>
+                    <Tab>ReactJS</Tab>
+                    <Tab>React Native</Tab>
+                </Tabs>
+                <section>
+                    <div className="content">Content for the tab: {this.state.activeTab}</div>
+                </section>
             </div>
-        )
+        );
     }
 }
 
