a7260a7dbe41c4895566f36b5cbc5afeca64a228
VolksRat71 <nathanieljryan1994@gmail.com>
2019-10-08 14:10:22 -0700

    contact page build going pretty well, still completing my right hand cell

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import {
+    Grid,
+    Cell,
+    Tabs,
+    List,
+    ListItem,
+    ListItemContent
+} from 'react-mdl';
+
+import img from '../assets/avatars/clean.png';
+
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Contact Page
-                </h1>
+            <div className="contact-body">
+                <Tabs className="contact-header" />
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                        <h2>
+                            Nathaniel Ryan
+                        </h2>
+                        <div className="contact-avatar-bkground">
+                            <img
+                                src={img}
+                                alt="contact-avatar"
+                                className="contact avatar"
+                            />
+                        </div>
+                        <p style={{ width: '75%', margin: 'auto', }}>
+                            IPhone offal af air plant photo booth selvage. Small batch vice hexagon, food truck adaptogen fingerstache blue bottle photo booth keytar hammock messenger bag. Lo-fi cold-pressed hammock +1. Helvetica cornhole wayfarers synth.
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>
+                            Contact Me
+                            </h2>
+                        <hr />
+                        <List>
+                            <ListItem>
+                                <ListItemContent icon="person">Bryan Cranston</ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent icon="person">Aaron Paul</ListItemContent>
+                            </ListItem>
+                            <ListItem>
+                                <ListItemContent icon="person">Bob Odenkirk</ListItemContent>
+                            </ListItem>
+                        </List>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
