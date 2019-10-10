d2c87ba7d79991ca48d22d419f3bc4cbbfd1e374
VolksRat71 <nathanieljryan1994@gmail.com>
2019-10-10 12:43:06 -0700

    about me content added

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -27,12 +27,13 @@ class About extends Component {
                             alt="hello"
                             className="hello"
                         />
+                        {/* 250 words */}
                         <p className="aboutMe_bio">
-                            Lorem ipsum dolor amet typewriter blue bottle leggings ennui, echo park tilde plaid af hashtag cold-pressed microdosing mustache mumblecore raw denim. Retro echo park vinyl salvia organic. Brooklyn blog stumptown, polaroid vaporware kale chips vape occupy austin crucifix adaptogen woke truffaut. Retro irony tattooed, cloud bread direct trade deep v kinfolk art party actually PBR&B aesthetic thundercats taxidermy. Woke hoodie hell of, paleo forage glossier leggings sartorial ramps vinyl squid authentic waistcoat.
-                        Gochujang crucifix kombucha godard activated charcoal. Semiotics fashion axe live-edge 3 wolf moon scenester photo booth, mumblecore air plant crucifix whatever normcore master cleanse la croix YOLO deep v. Mlkshk echo park tilde typewriter, affogato pok pok cloud bread normcore meh authentic poke mixtape readymade. Literally pork belly jianbing twee. Church-key tumeric letterpress humblebrag prism bespoke.
-                        Beard cliche slow-carb pork belly taiyaki. Seitan dreamcatcher organic intelligentsia, flannel venmo lyft PBR&B woke tumeric poke +1. Etsy VHS street art truffaut hexagon lo-fi cliche. Waistcoat godard scenester, kickstarter chicharrones letterpress fanny pack subway tile air plant slow-carb irony. Tofu PBR&B quinoa pitchfork tote bag. Marfa deep v palo santo crucifix 3 wolf moon.
-                        Unicorn coloring book godard semiotics seitan waistcoat tofu. Disrupt sriracha hashtag knausgaard, ramps asymmetrical seitan tofu gluten-free pug. Godard seitan banjo, tofu truffaut listicle shabby chic etsy tacos tumeric cold-pressed. Slow-carb subway tile deep v polaroid hexagon church-key, man braid brunch pork belly literally kitsch.
-                        Organic ugh cornhole edison bulb post-ironic occupy synth fixie chia. La croix asymmetrical
+                            I am from a small coal town in the Midwest. I moved to the West Coast to pursue a career in technology and computer sciences. I am currently a free agent
+                            with a formal education through the University of Oregon. My studies have given me proficiency in a wide array of technologies and design capabilities, such as ReactJS,
+                            NodeJS, and JavaScript in general. I am interested in following whichever development path my career points me to, although I do have a keen eye for UI/UX.
+                            Discovery of this ability has pivoted my focus to backend development, because I realized it is something I need to work on. I am a quick learner and very passionate,
+                            whether it be cars or computers. Knowing "how it works" is my forte. Please reach out; employers, designers, students and all. I am excited to hear from you!
                         </p>
                     </Cell>
                 </Grid>
