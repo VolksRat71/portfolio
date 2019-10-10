3fb97523abbab57d4bbc85c0e54119c9e7e2ff0b
VolksRat71 <nathanieljryan1994@gmail.com>
2019-10-09 18:18:02 -0700

    some content added to resume, dummy text for programming experience descriptions

--- src/components/resume.js
+++ src/components/resume.js
@@ -11,114 +11,117 @@ class Resume extends Component {
         return (
             <div>
                 <Tabs className="resume-header" />
-                <Grid>
-                    <Cell col={10}>
-                        <div id="cv" class="instaFade">
-                            <div class="mainDetails">
-                                <div id="name">
-                                    <h1 class="quickFade delayTwo">Nathaniel J. Ryan</h1>
-                                    <h2 class="quickFade delayThree">Jr. Developer</h2>
-                                </div>
-
-                                <div id="contactDetails" class="quickFade delayFour">
-                                    <ul>
-                                        <li>e: <a href="mailto:joe@bloggs.com" target="_blank">nathanieljryan1994@gmail.com</a></li>
-                                        <li>w: <a href="http://localhost:3000/resume">www.nathanielryan.com</a></li>
-                                        <li>m: <a href="tel:123-456-7890"> (503) 504-7581</a></li>
-                                    </ul>
-                                </div>
-                                <div class="clear"></div>
-                            </div>
-
-                            <div id="mainArea" class="quickFade delayFive">
-                                <section>
-                                    <article>
-                                        <div class="sectionTitle">
-                                            <h1>Personal Objective</h1>
-                                        </div>
-
-                                        <div class="sectionContent">
-                                            <p>A fresh candidate seeking entry level development employment or internships with a progressive company in order to gain valuable skills and experience for career growth.</p>
-                                        </div>
-                                    </article>
-                                    <div class="clear"></div>
-                                </section>
-
-
-                                <section>
-                                    <div class="sectionTitle">
-                                        <h1>Programming Experience</h1>
+                <div className="resume-background">
+                    <Grid>
+                        <Cell className="resume" col={10}>
+                            <div className="cv instaFade">
+                                <div className="mainDetails">
+                                    <div className="name">
+                                        <h1 className="quickFade delayTwo">Nathaniel J. Ryan</h1>
+                                        <h2 className="quickFade delayThree">Jr. Developer</h2>
                                     </div>
 
-                                    <div class="sectionContent">
-                                        <article>
-                                            <h2>Job Title at Company</h2>
-                                            <p class="subDetails">April 2011 - Present</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies massa et erat luctus hendrerit. Curabitur non consequat enim. Vestibulum bibendum mattis dignissim. Proin id sapien quis libero interdum porttitor.</p>
-                                        </article>
-
-                                        <article>
-                                            <h2>Job Title at Company</h2>
-                                            <p class="subDetails">Janruary 2007 - March 2011</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies massa et erat luctus hendrerit. Curabitur non consequat enim. Vestibulum bibendum mattis dignissim. Proin id sapien quis libero interdum porttitor.</p>
-                                        </article>
-
-                                        <article>
-                                            <h2>Job Title at Company</h2>
-                                            <p class="subDetails">October 2004 - December 2006</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies massa et erat luctus hendrerit. Curabitur non consequat enim. Vestibulum bibendum mattis dignissim. Proin id sapien quis libero interdum porttitor.</p>
-                                        </article>
-                                    </div>
-                                    <div class="clear"></div>
-                                </section>
-
-
-                                <section>
-                                    <div class="sectionTitle">
-                                        <h1>Key Skills</h1>
-                                    </div>
-
-                                    <div class="sectionContent">
-                                        <ul class="keySkills">
-                                            <li>A Key Skill</li>
-                                            <li>A Key Skill</li>
-                                            <li>A Key Skill</li>
-                                            <li>A Key Skill</li>
-                                            <li>A Key Skill</li>
-                                            <li>A Key Skill</li>
-                                            <li>A Key Skill</li>
-                                            <li>A Key Skill</li>
+                                    <div className="contactDetails quickFade delayFour">
+                                        <ul>
+                                            <li>e: <a href="mailto:joe@bloggs.com" target="_blank">nathanieljryan1994@gmail.com</a></li>
+                                            <li>w: <a href="http://localhost:3000/resume">www.nathanielryan.com</a></li>
+                                            <li>m: <a href="tel:123-456-7890"> (503) 504-7581</a></li>
                                         </ul>
                                     </div>
-                                    <div class="clear"></div>
-                                </section>
+                                    <div className="clear"></div>
+                                </div>
 
-
-                                <section>
-                                    <div class="sectionTitle">
-                                        <h1>Education</h1>
-                                    </div>
-
-                                    <div class="sectionContent">
+                                <div className="mainArea quickFade delayFive">
+                                    <section>
                                         <article>
-                                            <h2>College/University</h2>
-                                            <p class="subDetails">Qualification</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies massa et erat luctus hendrerit. Curabitur non consequat enim.</p>
-                                        </article>
+                                            <div className="sectionTitle">
+                                                <h1>Personal Objective</h1>
+                                            </div>
 
-                                        <article>
-                                            <h2>College/University</h2>
-                                            <p class="subDetails">Qualification</p>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies massa et erat luctus hendrerit. Curabitur non consequat enim.</p>
+                                            <div className="sectionContent">
+                                                <p>A fresh candidate seeking entry level development employment or internships with a progressive company in order to gain valuable skills and experience for career growth.</p>
+                                            </div>
                                         </article>
-                                    </div>
-                                    <div class="clear"></div>
-                                </section>
+                                        <div className="clear"></div>
+                                    </section>
 
+
+                                    <section>
+                                        <div className="sectionTitle">
+                                            <h1>Programming Experience</h1>
+                                        </div>
+
+                                        <div className="sectionContent">
+                                            <article>
+                                                <h2>Drone Controller</h2>
+                                                <p className="subDetails">ReactJS, NodeJS</p>
+                                                <p>Retro coloring book mustache semiotics street art typewriter XOXO kinfolk artisan. Banjo glossier edison bulb retro raclette YOLO. Meditation letterpress bushwick, humblebrag knausgaard shoreditch irony prism post-ironic migas lumbersexual roof party chambray. </p>
+                                            </article>
+
+                                            <article>
+                                                <h2>Emit Mobile App</h2>
+                                                <p className="subDetails">React Native, Firebase</p>
+                                                <p>Kickstarter gastropub stumptown. Photo booth blue bottle listicle irony, subway tile normcore kickstarter. Keffiyeh tumblr offal raclette, helvetica retro coloring book mustache juice gochujang irony cray.</p>
+                                            </article>
+
+                                            <article>
+                                                <h2>Seller2Sellar</h2>
+                                                <p className="subDetails">NodeJS, MySQL, Handlebars</p>
+                                                <p>Viral shoreditch roof party cold-pressed chartreuse tofu pug jianbing. Man braid scenester seitan venmo craft beer occupy 90's tbh. Swag mustache franzen hashtag helvetica.</p>
+                                            </article>
+                                        </div>
+                                        <div className="clear"></div>
+                                    </section>
+
+
+                                    <section>
+                                        <div className="sectionTitle">
+                                            <h1>Key Skills</h1>
+                                        </div>
+
+                                        <div className="sectionContent">
+                                            <ul className="keySkills">
+                                                <li>HTML5</li>
+                                                <li>CSS3</li>
+                                                <li>JavaScript</li>
+                                                <li>ReactJS</li>
+                                                <li>React Native</li>
+                                                <li>NodeJS</li>
+                                                <li>Express</li>
+                                                <li>MongoDB</li>
+                                                <li>MySQL</li>
+                                                <li>Git</li>
+                                            </ul>
+                                        </div>
+                                        <div className="clear"></div>
+                                    </section>
+
+
+                                    <section>
+                                        <div className="sectionTitle">
+                                            <h1>Education</h1>
+                                        </div>
+
+                                        <div className="sectionContent">
+                                            <article>
+                                                <h2>University of Oregon</h2>
+                                                <p className="subDetails">Full Stack Web Development</p>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies massa et erat luctus hendrerit. Curabitur non consequat enim.</p>
+                                            </article>
+
+                                            <article>
+                                                <h2>Westwood High School</h2>
+                                                <p className="subDetails">General Studies</p>
+                                                <p>Graduated in 2012</p>
+                                            </article>
+                                        </div>
+                                        <div className="clear"></div>
+                                    </section>
+                                </div>
                             </div>
-                        </div>
-                    </Cell>
-                </Grid>
+                        </Cell>
+                    </Grid>
+                </div>
             </div>
         )
     }
